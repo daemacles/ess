3b05d6d98c4ab40dfde8b0286adf4e91f84f2dc8
perlarssn <perlarssn@gmail.com>
2013-02-06 16:13:50 -0800

    Modified keyboard input. It now starts the rocket engine on UP-key

--- src/keyboardinput.cpp
+++ src/keyboardinput.cpp
@@ -1,5 +1,8 @@
 #include "keyboardinput.h"
 
+#include "rocketcontrol.h"
+#include "rocket.h"
+
 KeyboardInput::KeyboardInput(EntityHandler* entityHandler) {
     this->entityHandler = entityHandler;
 }
@@ -13,7 +16,12 @@ bool KeyboardInput::eventFilter(QObject *obj, QEvent *event)
         switch(KeyEvent->key())
         {
             case Qt::Key_Up:
-                Rocket* rocket = static_cast<Rocket*>(entityHandler->dynamicEnts["rocket"]);
+                {
+                    Rocket* rocket = static_cast<Rocket*>(entityHandler->dynamicEnts["rocket"]);
+                    RocketControl control = {0.205, 0.205, 0.205, 0.205, 0, 0, 0, 0};
+                    rocket->applyControl(control);
+                    return false;
+                }
                     /*
                 for(auto o : this->entityHandler->dynamicEnts) {
 
@@ -28,7 +36,6 @@ bool KeyboardInput::eventFilter(QObject *obj, QEvent *event)
 
                 }
                     */
-                return false;
             default:
                 break;
         }
