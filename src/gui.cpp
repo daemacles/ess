bc4d16909b72bc41de01690e4d12ac093a5688bb
perlarssn <perlarssn@gmail.com>
2013-02-07 20:05:24 -0800

    Disabled simulation and keyboard

--- src/gui.cpp
+++ src/gui.cpp
@@ -194,7 +194,9 @@ void GUI::draw() {
     // canvas
 
     // Update physics
-    this->simulator->stepSimulation(1./60.);
+    //this->simulator->stepSimulation(1./60.);
+    //
+
 
     glClearColor(0.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -232,7 +234,11 @@ void GUI::draw() {
     this->glCanvas->updateGL();
 
     // Listen for keyboard
+    /*
+    delete keyboardInput;
+    keyboardInput = new KeyboardInput(this->entityHandler);
     QCoreApplication::instance()->installEventFilter(keyboardInput);
+    */
 }
 
 void GUI::drawPoseHistory() {
